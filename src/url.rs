bd4a1957009ffe1a7f96973c4c785ac9cc11e0e1
figsoda <figsoda@pm.me>
2023-02-18 10:30:34 -0500

    unbreak url parsing and add tests

--- src/url.rs
+++ src/url.rs
@@ -38,7 +38,7 @@ pub(crate) fn parser<E: Error<char> + 'static>() -> impl Parser<char, String, Er
         .chain(just('.'))
         .chain(octet);
     let h16 = hex.repeated().at_least(1).at_most(4);
-    let h16r = h16.chain(just(':')).repeated();
+    let h16r = just(':').chain(h16).repeated();
     let ls32 = h16.chain(just(':')).chain(h16).or(ipv4);
 
     let segments = just('/').chain(pchar.repeated()).repeated().flatten();
@@ -48,21 +48,27 @@ pub(crate) fn parser<E: Error<char> + 'static>() -> impl Parser<char, String, Er
         .or_else(|_| Ok(Vec::new()))
         .chain::<char, _, _>(just('#').chain(frag).or_else(|_| Ok(Vec::new())));
 
-    let path = just('/')
-        .chain(
-            just('/')
-                .chain(
-                    percent
-                        .or(c)
-                        .repeated()
-                        .chain(just('@'))
-                        .or_else(|_| Ok(Vec::new())),
-                )
-                .chain::<char, _, _>(choice((
-                    reg.repeated(),
-                    ipv4,
-                    choice((
-                        h16r.exactly(6).flatten().chain(ls32),
+    let path = just('/').chain(
+        just('/')
+            .chain(
+                percent
+                    .or(set!(c!() | ':'))
+                    .repeated()
+                    .chain(just('@'))
+                    .or_else(|_| Ok(Vec::new())),
+            )
+            .chain::<char, _, _>(choice((
+                just('[')
+                    .chain(choice((
+                        just('v')
+                            .chain(hex.repeated().at_least(1))
+                            .chain(just('.'))
+                            .chain(set!(c!() | ':').repeated().at_least(1)),
+                        h16.chain(just(':'))
+                            .repeated()
+                            .exactly(6)
+                            .flatten()
+                            .chain(ls32),
                         just(':')
                             .chain(just(':'))
                             .chain::<char, _, _>(h16r.exactly(5).flatten())
@@ -80,65 +86,121 @@ pub(crate) fn parser<E: Error<char> + 'static>() -> impl Parser<char, String, Er
                             .chain(just(':'))
                             .chain::<char, _, _>(h16r.exactly(3).flatten())
                             .chain(ls32),
-                        h16r.at_most(2)
-                            .flatten()
-                            .chain(h16)
+                        h16.chain(h16r.at_most(2).flatten())
                             .or_not()
                             .chain(just(':'))
                             .chain(just(':'))
                             .chain::<char, _, _>(h16r.exactly(2).flatten())
                             .chain(ls32),
-                        h16r.at_most(3)
-                            .flatten()
-                            .chain(h16)
+                        h16.chain(h16r.at_most(3).flatten())
                             .or_not()
                             .chain(just(':'))
                             .chain(just(':'))
                             .chain::<char, _, _>(h16)
                             .chain(just(':'))
                             .chain(ls32),
-                        h16r.at_most(4)
-                            .flatten()
-                            .chain(h16)
+                        h16.chain(h16r.at_most(4).flatten())
                             .or_not()
                             .chain(just(':'))
                             .chain(just(':'))
                             .chain(ls32),
-                        h16r.at_most(5)
-                            .flatten()
-                            .chain(h16)
+                        h16.chain(h16r.at_most(5).flatten())
                             .or_not()
                             .chain(just(':'))
                             .chain(just(':'))
                             .chain(h16),
-                        h16r.at_most(6)
-                            .flatten()
-                            .chain(h16)
+                        h16.chain(h16r.at_most(6).flatten())
                             .or_not()
                             .chain(just(':'))
                             .chain(just(':')),
-                        just('v')
-                            .chain(hex.repeated().at_least(1))
-                            .chain(just('.'))
-                            .chain(set!(c!() | ':').repeated().at_least(1)),
-                    ))
-                    .delimited_by(just('['), just(']')),
-                )))
-                .chain::<char, _, _>(just(':').chain(digit.repeated()).or_not())
+                        // filter(|_| true).map(|_| todo!()),
+                    )))
+                    .chain(just(']')),
+                ipv4,
+                reg.repeated(),
+            )))
+            .chain::<char, _, _>(just(':').chain(digit.repeated()).or_not())
+            .chain(segments)
+            .or(pchar
+                .repeated()
+                .at_least(1)
                 .chain(segments)
-                .or_not(),
-        )
-        .or_else(|_| Ok(Vec::new()));
+                .or_else(|_| Ok(Vec::new())))
+            .or_not(),
+    );
 
     filter(char::is_ascii_alphabetic)
         .chain(set!('A' ..= 'Z' | 'a' ..= 'z' | '0' ..= '9' | '+' | '-' | '.').repeated())
         .chain(just(':'))
         .chain::<char, _, _>(path.or(pchar.repeated().at_least(1).chain(segments)))
-        .or(path.or(percent
+        .or(path)
+        .or(percent
             .or(set!(c!() | '@'))
             .repeated()
             .at_least(1)
-            .chain(segments)))
+            .chain(segments))
         .chain::<char, _, _>(frags)
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use chumsky::{prelude::Simple, primitive::end, Parser};
+
+    use super::parser;
+
+    fn parse(s: &str) -> Result<String, Vec<Simple<char>>> {
+        parser().then_ignore(end()).parse(s)
+    }
+
+    fn check(urls: impl IntoIterator<Item = impl AsRef<str>>) {
+        for url in urls {
+            let url = url.as_ref();
+            assert_eq!(url, parse(url).expect(url));
+        }
+    }
+
+    #[test]
+    fn basic() {
+        check([
+            "https://github.com/figsoda/pep-508",
+            "https://crates.io/search?q=pep-508&sort=recent-downloads",
+            "http://127.0.0.1:8000?some=query#anchor",
+            "/relative/url?query=good",
+            "another/relative/url#this",
+        ]);
+
+        assert!(parse("").is_err());
+        assert!(parse("https://example.com ").is_err());
+    }
+
+    // examples from https://datatracker.ietf.org/doc/html/rfc3986.htm
+    #[test]
+    fn examples() {
+        check([
+            "ftp://ftp.is.co.za/rfc/rfc1808.txt",
+            "http://www.ietf.org/rfc/rfc2396.txt",
+            "ldap://[2001:db8::7]/c=GB?objectClass?one",
+            "mailto:John.Doe@example.com",
+            "news:comp.infosystems.www.servers.unix",
+            "tel:+1-816-555-1212",
+            "telnet://192.0.2.16:80/",
+            "urn:oasis:names:specification:docbook:dtd:xml:4.1.2",
+        ]);
+    }
+
+    #[test]
+    fn ipv6() {
+        check([
+            "https://[::]",
+            "https://[::1]",
+            "https://[0:0:0:0:0:0:0:0]",
+            "https://[ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff]",
+        ]);
+
+        assert!(parse("[::]").is_err());
+        assert!(parse("https://[:::1]").is_err());
+        assert!(parse("https://[ffff:ffff:ffff:ffff:ffff:ffff:ffff]").is_err());
+        assert!(parse("https://[0:0:0:0:0:0:0:0:0]").is_err());
+    }
 }
